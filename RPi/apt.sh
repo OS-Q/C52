596fe6fc7d93c4b25b3b322d0a11819b9e1fcfe4
qitas <qitas@qitas.cn>
2018-09-11 14:22:11 +0800

    fix apt

--- RPi/apt.sh
+++ RPi/apt.sh
@@ -1,9 +1,9 @@
 
-sudo mv /etc/apt/sources.list /etc/apt/sources.list.origin
-sudo mv /etc/apt/sources.d/raspi.list /etc/apt/sources.d/raspi.list.origin
+sudo mv /etc/apt/sources.list  /etc/apt/sources.list.origin
+sudo mv /etc/apt/sources.list.d/raspi.list  /etc/apt/sources.list.d/raspi.list.origin
 
-sudo cp ./apt/sources.list /etc/apt/sources.list
-sudo cp ./apt/raspi.list /etc/apt/sources.d/raspi.list
+sudo cp ./apt/sources.list /etc/apt/
+sudo cp ./apt/raspi.list /etc/apt/sources.list.d/
 
 sudo apt update
-sudo apt upgrade
+sudo apt upgrade -y
